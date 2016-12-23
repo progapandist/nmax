93eca121326a77ef3239ecbc0f0824e2b6122e03
Andy B <andybarnov@gmail.com>
2016-12-23 18:29:09 +0300

    add some tests

--- spec/nmax_spec.rb
+++ spec/nmax_spec.rb
@@ -2,22 +2,17 @@ require "spec_helper"
 
 describe Nmax do
   let(:test_file_path) { File.dirname(__FILE__) + '/fixtures/test.txt' }
+  let(:text) { File.open(test_file_path) }
 
-  it "has a version number" do
-    expect(Nmax::VERSION).not_to be nil
+  it ".nmax outputs an array of integers" do
+    expect(subject.nmax(5, text).any? { |a| a.class == Fixnum }).to be_truthy
   end
 
-  it "correctly finds numbers in text" do
-    expect(subject.nmax(5, test_file_path)).to eq(['2'])
+  it ".nmax outputs n elements" do
+    expect(subject.nmax(3, text).count).to eq(3)
   end
-end
 
-# REMOVE LATER!
-describe "standard input" do
-  it "should receive `foobar`" do
-    fake_stdin("foobar") do
-      input = gets.to_s.chomp.strip
-      input.should == "foobar"
-    end
+  it ".nmax correctly picks integers from stream" do
+    expect(subject.nmax(5, text)).to eq([454543545454545, 453534545, 532545, 434343, 4545])
   end
 end
